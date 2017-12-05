ddad0c7371cd3fbbb839c5d32fcfbedbad82dfc9
Nikolas Gamarra <nxgamarra@gmail.com>
2017-12-05 18:03:26 -0500

    working baseline return value

--- Project3/Project3.py
+++ Project3/Project3.py
@@ -4,66 +4,147 @@
 import time
 import sys
 import math
-from memory_profiler import memory_usage
-from memory_profiler import profile
+#from memory_profiler import memory_usage
+#from memory_profiler import profile
+import itertools
+from itertools import combinations
+from itertools import permutations
 
-global carryCapacity = 6
-@profile
-def ES(weights,values,array):# Exhaustive Search
-    global carryCapacity
-    i=1
+def ES(carryCapacity,weights,values):
     n=len(weights)
-    while i < 2**n:
-        j=n
-        tempWeight=0
-        tempValue=0
-        while(array[j]!=0 and j>0):
-            array[j]=0
-            j=j-1
-        array[j]=1
-        for k range(1 to n):
-            if(array[k]==1:
-                    tempWeight=tempWeight+weights[k]
-                    temValue=tempValue+values[k]
-        if(tempValue>bestValue and tempWeight <= carryCapacity):
-            bestValue=tempValue
-            bestWeight=tempWeight
+    itemResult=[]
+    x=0
+    array=[]
+    lastAttempt=[]
+    while x < n:
+        w=int(weights[x])
+        v=int(values[x])
+        tup=(w,v)
+        array.append(tup)
+        x=x+1
+    perm=list(permutations(array))
+    highestValue=0
 
-        result=array
+    highestWeight=0
+    x=0
+    #for attempt in list(perm):#for the possible configurations in all permutations
+    while x< len(perm):
+        attempt=perm[x]
+        x=x+1
+       # print(attempt)
+        itemIndex=0
+        runningWeight=0
+        runningValue=0
+        print("NEW ATTEMPT______________________")
+        print(attempt)
+
+        while itemIndex<n:#for the items in this attempt
+
+            print(itemIndex)
+            if(int(int(attempt[itemIndex][0])+runningWeight)<=int(carryCapacity)):
+                runningWeight=runningWeight+int(attempt[itemIndex][0])#add the weight
+                runningValue=runningValue+int(attempt[itemIndex][1])#add the value
+
+                if(runningValue>=highestValue):
+                    print('new highest******************************************************************')
+                    itemResult=attempt
+                    highestValue=runningValue
+                    highestWeight=runningWeight
+
+                    #del attempt[itemIndex:]
+
+                    itemResult=attempt[0:itemIndex+1]
+                    #i=0
+                    #while( i < n):
+                    #    itemResult.append(attempt[i])
+                    #    i=i+1
+            print('test')
+            print('capacity: ' + str(carryCapacity))
+            print('runningWeight: ' + str(runningWeight))
+            print('  runningValue:  ' + str(runningValue))
+            print('  highest val: ' + str(highestValue))
+
+            itemIndex=itemIndex+1
+    result=(itemResult,highestValue,highestWeight)
     return result
 
-@profile
-def DP(items):#Dynamic Programming
-    global carryCapacity
-    return
+#@profile
+def DP(carryCapacity,weights,values):#dynamic programming
+    n=len(weights)
+    K = [[0 for x in range(int(carryCapacity) + 1)] for x in range(n + 1)]
+    # Build table K[][] in bottom up manner
+    items=[]
+    for i in range(n + 1):
+        for w in range(carryCapacity + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif int(weights[i - 1]) <= w:
+                K[i][w] = max(int(values[i - 1]) + K[i - 1][w - int( weights[i - 1])], K[i - 1][w])
 
-@profile
-def MY(items):#Alternative Function
-    global carryCapacity
-    return
+            else:
+                K[i][w] = K[i - 1][w]
 
-def testAll(data):#function to test all 3 functions
+    return values,K[n][carryCapacity],values
+
+
+#@profile
+def MY(carryCapacity,weights,values,n):#Alternative Function
+    # Returns the maximum value that can be put in a knapsack of
+    # capacity W
+#def knapSack(W, wt, val, n):
+    # Base Case
+
+    if n == 0 or carryCapacity == 0:
+        return 0
+
+    # If weight of the nth item is more than Knapsack of capacity
+    # W, then this item cannot be included in the optimal solution
+    if (int(weights[n - 1]) > carryCapacity):
+        return MY(carryCapacity, weights, values, n - 1)
+
+    # return the maximum of two cases:
+    # (1) nth item included
+    # (2) not included
+    else:
+        return max(values[n - 1] + MY(carryCapacity - weights[n - 1], weights, values, n - 1),MY(carryCapacity, weights, values, n - 1))
+
+
+def testAll(carryCapacity,weights,values):#function to test all 3 functions
+    carryCapacity=int(carryCapacity)
     print('\nTesting Exhaustive Search\n')
     t0=time.time()
-    result=ES(data)
+    resultES=ES(carryCapacity,weights,values)
     t1=time.time()
     T=t1-t0
-    print('The following items were selected: '+str(result)+' Answer was calculated in time: '+str(T)+' seconds.\n')
+    print('The following items were selected: '+str(resultES[0]))
+    print('Value: '+str(resultES[1]))
+    print('Weight: '+str(resultES[2]))
+    print('The answer was calculated in time: '+str(T)+' seconds.\n')
+    print(str(T))
 
     print('\nTesting Dynamic Programming\n')
     t0=time.time()
-    result=DP(data)
+    resultDP=DP(carryCapacity,weights,values)
     t1=time.time()
     T=t1-t0
-    print('The following items were selected: '+str(result)+' Answer was calculated in time: '+str(T)+' seconds.\n')
+    #print(resultDP)
+
+    print('The following items were selected: ' + str(resultDP[0]))
+    print('Value: ' + str(resultDP[1]))
+    #print('Weight: ' + str(resultDP[2]))
+    print('The answer was calculated in time: ' + str(T) + ' seconds.\n')
+    print(str(T))
 
     print('\nTesting My Function\n')
     t0=time.time()
-    result=MY(data)
+    resultMY=MY(carryCapacity,weights,values,int(len(values)))
     t1=time.time()
     T=t1-t0
-    print('The following items were selected: '+str(result)+' Answer was calculated in time: '+str(T)+' seconds.\n')
-
+    #print('The following items were selected: ' + str(result[0]))
+    print('Value: ' + str(resultMY))
+    print('Weight: ' + str(resultMY))
+    print('The answer was calculated in time: ' + str(T) + ' seconds.\n')
+    print(str(T))
     return
 
 
@@ -75,43 +156,47 @@ if __name__ == '__main__':
     if len(sys.argv)>1:
         filename=str(sys.argv[1])
     else:
-        filename="input.txt"
+        filename="input-1.txt"
     try:  # check of invalid inputs
         f = open(filename, "r")
         array = f.read()
-        for char in "][":
-            array = array.replace(char, '')
-        array.split("),(")
-        for char in ")(":
-            array = array.replace(char, '')
-        array.split(",")
-        array = array.split(',')
-        if len(array) % 2 != 0:#check that there is an even nuber of numbers in the array
-            raise ValueError
+        weights=[]
+        values=[]
+        tempArray=[]
+        carryCapacitiy=0
+
+        tempArray=array.splitlines(0)
+        carryCapacity=tempArray[0]
+        weights=tempArray[1]
+        values=tempArray[2]
+
+        weights = weights.split(',')
+        values = values.split(',')
+
+        #for w in weights:
+        #    w=int(w)
+        #for v in values:
+        #    v=int(v)
+        Ivalues = [int(v) for v in values]
+        Iweights = [int(w) for w in weights]
+
+        #if len(array) % 2 != 0:#check that there is an even nuber of numbers in the array
+        #    raise ValueError
         i = 0
-        newArray = []
-        while i < len(array):#run through the array
-            x = int(array[i])#extract x as an int
-            y = int(array[i + 1])#extract y as an int
-            point = (x, y)# put into tupl
-            newArray.append(point)#add tupl to array 
-            i = i + 2
+        if len(weights) != len(values):
+            raise ValueError
+
     except IOError:
-        print'Could no read file:', filename  # Primpt user to correct their input
+        print ("Could no read file:"+str(filename))# Primpt user to correct their input
         sys.exit()
     except ValueError:
-        print "Value Error: make sure input is valid"
-        sys.exit()
-    except UserError:
-        print "invalid number of inputs"
+        print ("Value Error: make sure input is valid")
         sys.exit()
     else:
         #break
-        print '\nThe input from ' +filename+' is: \n'
-        print newArray
-        print '\nLength: '+str(len(newArray))
-        testAll(newArray)
-'''
-class MyException(Exception):
-    pass
-raise MyException
+        print ('\nThe input from ' +filename+' is: ')
+        print ('   Carry Capacity: ' +str(carryCapacity))
+        print ('   Weights: ' +str(weights))
+        print ('   Values:  ' +str(values))
+        print ('   Number of items: '+str(len(weights)))
+        testAll(carryCapacity,Iweights,Ivalues)
